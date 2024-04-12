3f9f0fc047029e8baa435ade52ec0cdbd93d6583
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-04-12 22:04:32 +0545

    Heading part and Expense Item styles

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,9 @@ import { ThemeSwitcher } from '@/components/ThemeSwitcher';
 export default function Home() {
   return (
     <>
-
       <main className="container border border-blue-200 h-screen w-full">
         {/* header */}
-        <header className='h-16  flex align-center shadow-orange-700/10 shadow-lg justify-between rounded-xl w-full p-4 '>
+        <header className='h-16  flex align-center shadow-gray-500/10 shadow-md justify-between w-full p-4 dark:shadow-gray-500/30 border dark:border-gray-600'>
           <span className='my-auto'><ThemeSwitcher /></span>
           <p className='font-medium text-lg hover:underline my-auto'>Your Expenses</p>
           <button className="Btn my-auto">
@@ -23,69 +22,50 @@ export default function Home() {
         </header>
 
         {/* Total Expense Viewer */}
-        <div className='h-48 w-full border border-red-400 mx-auto my-4 '>
-          <div className='flex align-center justify-center relative m-14'>
+        <div className='flex flex-col border-b-4 border-gray-200 dark:border-gray-600  h-38 w-full mx-auto '>
+          <div className='text-xs w-full text-center mx-auto pb-4 italic tracking-widest'> <span className='text-xl text-red-500'>" </span>Track Your Money: Take Charge of Your Finances <span className='text-xl text-red-500'>" </span></div>
+          <div className='flex justify-center align-center'>
             <span className='text-2xl text-red-600  mb-14 my-auto'>Rs. </span>
             <div className='text-7xl font-bold  p-4'>550</div>
           </div>
+          <span className='py-4 text-center align-center tracking-widest text-gray-500'>12 April, 2024</span>
         </div>
 
         {/* List of the expenses */}
-        <div className='flex flex-grow flex-col gap-4 p-4 border border-red-800  w-full h-2/3 overflow-y-scroll pb-24 '>
-          <li className='flex justify-between border-b border-gray-300 p-4'>
-            <div>Education</div>
-            <div>- Rs. 3000</div>
+        <div className='shadow-gray-500/10 shadow-xl text-base shadow-md text-gray-500 dark:text-gray-400 flex justify-evenly px-6 py-4  '>
+          <span>This Month's total </span>
+          <div> <span className='text-red-500'> Rs. </span>5000</div>
+        </div>
+        <div className='flex flex-col gap-4 w-full h-2/4 overflow-y-scroll '>
+          <li className='mt-4 text-gray-700 flex justify-between border-b border-gray-300 px-8 py-4 dark:text-gray-300 dark:border-gray-700'>
+            <div className='font-bold text-base'> <span className='text-xl mr-2'> 🍔 </span>  Education</div>
+            <div className='text-lg'>- <span className='text-red-500 dark:text-red-500'>Rs. </span>3000</div>
           </li>
-          <li className='flex justify-between border-b border-gray-300 p-4'>
-            <div>Education</div>
-            <div>- Rs. 3000</div>
+          <li className='text-gray-700 flex justify-between border-b border-gray-300 px-8 py-4 dark:text-gray-300 dark:border-gray-700'>
+            <div className='font-bold text-base'> <span className='text-xl mr-2'> 🍔 </span>  Education</div>
+            <div className='text-lg'>- <span className='text-red-500 dark:text-red-500'>Rs. </span>3000</div>
           </li>
-          <li className='flex justify-between border-b border-gray-300 p-4'>
-            <div>Education</div>
-            <div>- Rs. 3000</div>
+          <li className='text-gray-700 flex justify-between border-b border-gray-300 px-8 py-4 dark:text-gray-300 dark:border-gray-700'>
+            <div className='font-bold text-base'> <span className='text-xl mr-2'> 🍔 </span>  Education</div>
+            <div className='text-lg'>- <span className='text-red-500 dark:text-red-500'>Rs. </span>3000</div>
           </li>
-          <li className='flex justify-between border-b border-gray-300 p-4'>
-            <div>Education</div>
-            <div>- Rs. 3000</div>
+          <li className='text-gray-700 flex justify-between border-b border-gray-300 px-8 py-4 dark:text-gray-300 dark:border-gray-700'>
+            <div className='font-bold text-base'> <span className='text-xl mr-2'> 🍔 </span>  Education</div>
+            <div className='text-lg'>- <span className='text-red-500 dark:text-red-500'>Rs. </span>3000</div>
           </li>
-          <li className='flex justify-between border-b border-gray-300 p-4'>
-            <div>Education</div>
-            <div>- Rs. 3000</div>
+          <li className='text-gray-700 flex justify-between border-b border-gray-300 px-8 py-4 dark:text-gray-300 dark:border-gray-700'>
+            <div className='font-bold text-base'> <span className='text-xl mr-2'> 🍔 </span>  Education</div>
+            <div className='text-lg'>- <span className='text-red-500 dark:text-red-500'>Rs. </span>3000</div>
           </li>
-          <li className='flex justify-between border-b border-gray-300 p-4'>
-            <div>Education</div>
-            <div>- Rs. 3000</div>
+          <li className='text-gray-700 flex justify-between border-b border-gray-300 px-8 py-4 dark:text-gray-300 dark:border-gray-700'>
+            <div className='font-bold text-base'> <span className='text-xl mr-2'> 🍔 </span>  Education</div>
+            <div className='text-lg'>- <span className='text-red-500 dark:text-red-500'>Rs. </span>3000</div>
           </li>
-          <li className='flex justify-between border-b border-gray-300 p-4'>
-            <div>Education</div>
-            <div>- Rs. 3000</div>
-          </li>
-          <li className='flex justify-between border-b border-gray-300 p-4'>
-            <div>Education</div>
-            <div>- Rs. 3000</div>
-          </li>
-          <li className='flex justify-between border-b border-gray-300 p-4'>
-            <div>Education</div>
-            <div>- Rs. 3000</div>
-          </li>
-          <li className='flex justify-between border-b border-gray-300 p-4'>
-            <div>Education</div>
-            <div>- Rs. 3000</div>
-          </li>
-          <li className='flex justify-between border-b border-gray-300 p-4'>
-            <div>Education</div>
-            <div>- Rs. 3000</div>
-          </li>
-          <li className='flex justify-between border-b border-gray-300 p-4'>
-            <div>Health</div>
-            <div>- Rs. 3000</div>
-          </li>
-
         </div>
 
 
         {/* footer section */}
-        <footer className='z-50  bg-white dark:bg-darkColor flex border border-gray-300 dark:border-gray-600 p-4 justify-between absolute left-0 bottom-0 w-full'>
+        <footer className='z-50 h-1/12 bg-white dark:bg-darkColor flex border border-gray-300 dark:border-gray-600 p-4 justify-around absolute left-0 bottom-0 w-full'>
           <div className='bg-orange-200  p-2 my-auto rounded-full dark:bg-orange-400'>
             <Image src={'/bar-chart.png'} width={25} height={25} alt='analytics'></Image>
           </div>
