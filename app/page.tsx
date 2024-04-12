4416dd6e1cf7d89d8199bfab60d661edb5c4c27d
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-04-12 23:34:49 +0545

    Header styles, popUp

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         {/* header */}
         <header className='h-16  flex align-center shadow-gray-500/10 shadow-md justify-between w-full p-4 dark:shadow-gray-500/30 border dark:border-gray-600'>
           <span className='my-auto'><ThemeSwitcher /></span>
-          <p className='font-medium text-lg hover:underline my-auto'>Your Expenses</p>
+          <p className='font-medium text-lg border-b-2 dark:border-gray-500 border-gray-300 my-auto'>My <span className='text-orange-600'> Expenses </span></p>
           <button className="Btn my-auto">
 
             <div className="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
@@ -28,7 +28,7 @@ export default function Home() {
 
         {/* Total Expense Viewer */}
         <div className='flex flex-col border-b-4 border-gray-200 dark:border-gray-600  h-38 w-full mx-auto '>
-          <div className='text-xs w-full text-center mx-auto pb-4 italic tracking-widest'> <span className='text-xl text-orange-500'>" </span>Track Your Money: Take Charge of Your Finances <span className='text-xl text-orange-500'>" </span></div>
+          <div className='text-xs w-full text-center mx-0 pb-4 italic tracking-widest'> <span className='text-xl text-orange-500'>" </span>Track Your Money: Take Charge of Your Finances <span className='text-xl text-orange-500'>" </span></div>
           <div className='flex justify-center align-center'>
             <span className='text-2xl text-orange-600  mb-14 my-auto'>Rs. </span>
             <div className='text-7xl font-bold  p-4'>550</div>
@@ -39,7 +39,7 @@ export default function Home() {
         {/* List of the expenses */}
         <div className='shadow-gray-500/10 shadow-xl text-base shadow-md text-gray-500 dark:text-gray-400 flex justify-evenly px-6 py-4  '>
           <span>This Month's total </span>
-          <div> <span className='text-orange-500'> Rs. </span>5000</div>
+          <div className='border-b-2 border-gray-400 dark:border-gray-500'> <span className='text-orange-500'> Rs. </span>5000</div>
         </div>
         <div className='flex flex-col gap-4 w-full h-2/4 overflow-y-scroll '>
           <li className='mt-4 text-gray-700 flex justify-between border-b border-gray-300 px-8 py-4 dark:text-gray-300 dark:border-gray-700'>
