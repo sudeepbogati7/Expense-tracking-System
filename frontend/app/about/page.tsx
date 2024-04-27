7d07d343f75fea001822e0443584b24b3be8d3cd
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-04-27 21:08:09 +0545

    About page

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -5,10 +5,23 @@ import React from 'react';
 import Header from '@/components/Header';
 import Image from 'next/image';
 import Link from 'next/link';
+import { ThemeSwitcher } from '@/components/ThemeSwitcher';
 export default function AboutMe() {
     return (
         <>
-            <div className="container flex items-center w-full mx-auto py-4 px-2 my-4 flex-col mx-auto border-b border-gray-400 ">
+            <header className="h-16 fixed z-50 bg-gray-200 dark:bg-gray-900 top-0 flex align-center shadow-gray-500/10 shadow-md justify-between w-full p-4 dark:shadow-gray-500/30">
+                <span className="my-auto">
+                    <ThemeSwitcher />
+                </span>
+                <Link href={"/"}>
+                    {" "}
+                    <p className="font-medium text-lg border-b-2 hover:border-orange-500 dark:hover:border-orange-500 transition-all duration-900  ease-linear dark:border-gray-500 border-gray-300 my-auto">
+                        My <span className="text-orange-600"> Expenses </span>
+                    </p>
+                </Link>
+                <button className='border border-gray-400 px-2 rounded-lg text-center hover:bg-orange-400 hover:text-white text-base active:bg-orange-800 active:text-white transition-all duration-300 ease'><a href="/"> Home </a> </button>
+            </header>
+            <div className="mt-14 container flex items-center w-full mx-auto py-4 px-2 my-4 flex-col mx-auto border-b border-gray-400 ">
                 <div className='flex gap-6 items-center p-2 border-b-2 border-gray-400'>
                     <div className='flex flex-col items-center '>
                         <div className='font-semibold tracking-wide text-xl border-b border-orange-400 text-orange-600 '>Sudeep Bogati</div>
@@ -63,7 +76,6 @@ export default function AboutMe() {
                     <span className='tracking-widest text-orange-700 text-sm'>hello@sudipbogati.com.np</span>
                 </div>
 
-                <Link href={'/'} className='w-fit  mx-auto border border-gray-300 px-4 py-2 mt-6 rounded-lg bg-gray-300 active:border-2 active:bg-orange-200 active:border-orange-700 transition-all duration-300 dark:bg-gray-800 dark:border-none dark:active:bg-orange-600'>Home</Link>
             </div>
             <div className='flex flex-col px-4'>
                 <h1 className='text-lg font-semibold border-b-2 mx-auto w-fit border-orange-400'> About Expense Tracker Project</h1>
@@ -75,7 +87,7 @@ export default function AboutMe() {
                     <li>Tracking daily and monthly expenses.</li>
                 </ul>
                 <span className='font-semibold text-base py-2'>Technologies Used:</span>
-                <div className='flex items-center justify-around rounded-xl bg-gray-50 dark:bg-gray-500'>
+                <div className='flex items-center justify-around rounded-xl bg-gray-50 border border-gray-400 dark:bg-gray-500'>
                     <span className='hover:bg-blue-500 rounded-full overflow-hidden'>
                         <svg height="30pt" viewBox=".5 -.2 1023 1024.1" width="30pt" xmlns="http://www.w3.org/2000/svg"><path d="m478.5.6c-2.2.2-9.2.9-15.5 1.4-145.3 13.1-281.4 91.5-367.6 212-48 67-78.7 143-90.3 223.5-4.1 28.1-4.6 36.4-4.6 74.5s.5 46.4 4.6 74.5c27.8 192.1 164.5 353.5 349.9 413.3 33.2 10.7 68.2 18 108 22.4 15.5 1.7 82.5 1.7 98 0 68.7-7.6 126.9-24.6 184.3-53.9 8.8-4.5 10.5-5.7 9.3-6.7-.8-.6-38.3-50.9-83.3-111.7l-81.8-110.5-102.5-151.7c-56.4-83.4-102.8-151.6-103.2-151.6-.4-.1-.8 67.3-1 149.6-.3 144.1-.4 149.9-2.2 153.3-2.6 4.9-4.6 6.9-8.8 9.1-3.2 1.6-6 1.9-21.1 1.9h-17.3l-4.6-2.9c-3-1.9-5.2-4.4-6.7-7.3l-2.1-4.5.2-200.5.3-200.6 3.1-3.9c1.6-2.1 5-4.8 7.4-6.1 4.1-2 5.7-2.2 23-2.2 20.4 0 23.8.8 29.1 6.6 1.5 1.6 57 85.2 123.4 185.9s157.2 238.2 201.8 305.7l81 122.7 4.1-2.7c36.3-23.6 74.7-57.2 105.1-92.2 64.7-74.3 106.4-164.9 120.4-261.5 4.1-28.1 4.6-36.4 4.6-74.5s-.5-46.4-4.6-74.5c-27.8-192.1-164.5-353.5-349.9-413.3-32.7-10.6-67.5-17.9-106.5-22.3-9.6-1-75.7-2.1-84-1.3zm209.4 309.4c4.8 2.4 8.7 7 10.1 11.8.8 2.6 1 58.2.8 183.5l-.3 179.8-31.7-48.6-31.8-48.6v-130.7c0-84.5.4-132 1-134.3 1.6-5.6 5.1-10 9.9-12.6 4.1-2.1 5.6-2.3 21.3-2.3 14.8 0 17.4.2 20.7 2z" /><path d="m784.3 945.1c-3.5 2.2-4.6 3.7-1.5 2 2.2-1.3 5.8-4 5.2-4.1-.3 0-2 1-3.7 2.1zm-6.9 4.5c-1.8 1.4-1.8 1.5.4.4 1.2-.6 2.2-1.3 2.2-1.5 0-.8-.5-.6-2.6 1.1zm-5 3c-1.8 1.4-1.8 1.5.4.4 1.2-.6 2.2-1.3 2.2-1.5 0-.8-.5-.6-2.6 1.1zm-5 3c-1.8 1.4-1.8 1.5.4.4 1.2-.6 2.2-1.3 2.2-1.5 0-.8-.5-.6-2.6 1.1zm-7.6 4c-3.8 2-3.6 2.8.2.9 1.7-.9 3-1.8 3-2 0-.7-.1-.6-3.2 1.1z" /></svg>
                     </span>
@@ -130,11 +142,11 @@ export default function AboutMe() {
                         <li>Enter your username or email and password to log in.</li>
                         <li>Upon successful login, a JWT token is generated and stored in an local storage in the client side.</li>
                     </ul>
-                   <li className='font-medium'>Authorization with JWT:</li>
+                    <li className='font-medium'>Authorization with JWT:</li>
                     <ul className='list-disc p-2'>
                         JWT tokens are used to verify the user identify for adding new expenses and fetching user informations for profile.
 
-                   </ul>
+                    </ul>
                 </ol>
                 <h2 className='text-lg font-semibold mb-2 font-semibold border-b-2 border-orange-500 w-fit'>Expense Tracking Features</h2>
                 <ol className='list-decimal px-4'>
@@ -174,15 +186,15 @@ export default function AboutMe() {
                 <h2 className='text-lg font-semibold mb-2 font-bold border-b-2 border-orange-500 w-fit'>Conclusion</h2>
                 <p>This expense tracker empowers you to take control of your finances. With its user-friendly interface, powerful visualization tools, and secure authentication, you can make informed spending decisions and achieve your financial goals.</p>
             </div>
-            <footer className=' border-t border-gray-400 bg-gradient-to-t from-[#BBA66D] via-[#D5D1C7] to-gray-200 px-2 py-4 mt-8 flex flex-col items-center '>
-                <span className='text-gray-900 font-semibold'>Suggestions for improvement on </span>
-                <span className='text-sm tracking-widest'>sudeepbogati080@gmail.com</span>
-                <span className='text-base font-medium pt-2 mb-2  border-b border-gray-600 text-gray-800'> Find me on </span>
-                <ul className='flex gap-2 w-full items-center justify-around list-disc'> 
-                    <li className='hover:underline text-gray-700'><Link href={'https://stackoverflow.com/users/24161634/sudeepbogati'}> StackOverflow </Link></li>
-                    <li className='hover:underline text-gray-700'><Link href={'https://github.com/sudeepbogati7'}>Github </Link></li>
-                    <li className='hover:underline text-gray-700'><Link href={'https://linkedin.com/in/sudeep-bogati'}> LinkedIn </Link></li>
-                    <li className='hover:underline text-gray-700'> <Link href={'https://www.instagram.com/the.sudeep_/'}> Instagram </Link></li>
+            <footer className=' border-t border-gray-400 bg-gradient-to-t from-[#BBA66D] via-[#D5D1C7] to-gray-200 px-2 py-4 mt-8 flex flex-col items-center dark:bg-gradient-to-t dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 '>
+                <span className='text-gray-900 dark:text-gray-300 font-semibold'>Suggestions for improvement on </span>
+                <span className='text-sm dark:text-gray-300 tracking-widest'>sudeepbogati080@gmail.com</span>
+                <span className='text-base font-medium pt-2 mb-2  border-b border-gray-600 text-gray-800 dark:text-gray-300'> Find me on </span>
+                <ul className='flex gap-2 w-full items-center justify-around list-disc'>
+                    <li className='hover:underline text-gray-700 dark:text-gray-400'><Link href={'https://stackoverflow.com/users/24161634/sudeepbogati'}> StackOverflow </Link></li>
+                    <li className='hover:underline text-gray-700 dark:text-gray-400'><Link href={'https://github.com/sudeepbogati7'}>Github </Link></li>
+                    <li className='hover:underline text-gray-700 dark:text-gray-400'><Link href={'https://linkedin.com/in/sudeep-bogati'}> LinkedIn </Link></li>
+                    <li className='hover:underline text-gray-700 dark:text-gray-400'> <Link href={'https://www.instagram.com/the.sudeep_/'}> Instagram </Link></li>
                 </ul>
             </footer>
         </>
