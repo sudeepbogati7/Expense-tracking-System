694a5ea994a8832c40542e3d7ea2340649525f59
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-05-01 00:55:49 +0545

    Category filtration, about page

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -5,7 +5,7 @@ import 'tailwindcss/tailwind.css';
 export default function Loading() {
     return (
         <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black/60 ">
-            <div className="loader">
+            <div className="loader dark:bg-gray-700">
                 <label className="loading-label">Please wait...</label>
                 <div className="loading"></div>
             </div>
