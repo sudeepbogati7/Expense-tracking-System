2ab61e8a108d507adcee443f1cca8616c0441c31
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-06-23 20:30:21 +0545

    Minor changes

--- frontend/components/ResponseDataContext.tsx
+++ frontend/components/ResponseDataContext.tsx
@@ -1,7 +1,5 @@
 'use client';
-
 import React, { createContext, useState, useContext } from 'react';
-
 interface User {
     userId: number;
     fullName: string;
@@ -16,23 +14,16 @@ interface ResponseData {
     user: User;
     token: string;
 }
-
-// Define an initial context value
 const initialContextValue: {
     responseData: ResponseData | null;
     setResponseData: React.Dispatch<React.SetStateAction<ResponseData | null>>;
 } = {
     responseData: null,
-    setResponseData: () => { } // Dummy function that does nothing
+    setResponseData: () => { }
 };
-
-// Create the context
 const ResponseDataContext = createContext(initialContextValue);
-
-// Custom hook to access the context value
 export const useResponseData = () => useContext(ResponseDataContext);
 
-// Context provider component
 export const ResponseDataProvider: any = ({ children }: any) => {
     const [responseData, setResponseData] = useState<ResponseData | null>(null);
 
