561ef9d3683f56edc0ec7de07982b04b3be39178
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-06-27 17:04:19 +0545

    cors update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,8 @@ import cookieParser from 'cookie-parser';
 app.use(bodyParser.json());
 app.use(Express.json());
 app.use(cors({
-    origin: 'https://expense-tracker-2u19v0qvk-sudeepbogati7s-projects.vercel.app/'
+    origin: 'https://expense-tracker-2u19v0qvk-sudeepbogati7s-projects.vercel.app/',
+    optionsSuccessStatus: 200
 }));
 
 
